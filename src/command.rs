8622a631207fd9a041b062af79e2a09a4bddf1b2
Great Ezenna <ezennagreat@gmail.com>
2026-08-16 11:44:33 +0100

    refactor: enhance lexer and error handling structure in command module

--- src/command.rs
+++ src/command.rs
@@ -1,16 +1,33 @@
-use std::string::ParseError;
+// use std::string::ParseError;
+use std::fmt;
+use std::error::Error;
 
 // will eventually make a smarter lexer, this should do for now - todo
-enum Token {
+#[derive(Debug)]
+pub enum Token {
     Word(String),
     QuotedString(String)
 }
 
+// this lexer currently has one way to fail: reaching end of line while still inside a quote
+#[derive(Debug)]
+pub enum LexError {
+    StillInQuote
+}
+
+impl fmt::Display for LexError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Reached EOL while still inside a quote")
+    }
+}
+
+impl Error for LexError {}
+
 
 pub fn lexer(line: &str) -> Result<Vec<Token>, LexError> {
-
+    
 }
 
-pub fn parse(tokens: &[Token]) -> Result<Command, ParseError> {
+// pub fn parse(tokens: &[Token]) -> Result<Command, ParseError> {
 
-}
+// }
