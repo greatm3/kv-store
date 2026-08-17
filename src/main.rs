5e07a2d8654080e94a1a6b55c400a94a00088154
Great Ezenna <ezennagreat@gmail.com>
2026-08-17 18:16:42 +0100

    refactor: remove debug code in main

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,6 @@
 mod command;
 mod store;
 
-use command::{Token, parse};
-
 fn main() {
-    let sp = vec![
-        Token::Word(String::from("set")),
-        Token::Word(String::from("GET")),
-    ];
-
-    let result = parse(&sp);
-
-    match result {
-        Ok(s) => println!("{:?}", s),
-        Err(e) => println!("{e}"),
-    }
+    
 }
